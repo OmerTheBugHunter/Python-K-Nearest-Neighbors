ebe1ad473afb1150e3dd8e87816f7e811115d749
OmerTheBugHunter <omeribrahimtoka77@gmail.com>
2025-04-01 19:29:08 +0300

    Update knearestneighbor.py

--- knearestneighbor.py
+++ knearestneighbor.py
@@ -1,4 +1,4 @@
-# KNN ve Accuracy Hesaplayıcı algoritmalarımızı kuralım.
+# Setup the KNN and Accuracy algorithms.
 import numpy as np
 from collections import Counter
 
@@ -18,24 +18,22 @@ def knn(X_train_dataset, X_test_dataset, y_train_dataset, k):
             dist = 0
             value = 0
             
-            for col in feature_columns:
+            for col in X_train_dataset.columns:
                 value = euc_dist(X_test_dataset.iloc[i][col], X_train_dataset.iloc[j][col])
                 dist += value
 
             dists.append((dist,y_train_dataset.iloc[j]['Class']))
 
-        # Fonksiyonun bu bölümünde elimizde 142 elemanlı bir dists listesi mevcut.
-        # Elimizdeki liste X_test_dataset'inin i.datasına ait mesafe ölçümleridir. Ancak mesafeler sıralanmamış durumdadır. Sıralayalım.
+        # We have a list which contains the distances of the i th data of the X_test_dataset. But these distances are not in an order.
         sorted_dists = sorted(dists, key=lambda x: x[0])
 
-        # Şimdi küçükten büyüğe sıralanmış listemizdeki ilk k elemanı alalım.
+        # Pick first k element of the sorted distance list.
         k_dists = [(float(a), int(b)) for a, b in sorted_dists[:k]]
 
-        # Elimizde k elemanlı bir tuple listesi mevcut. 
-        # Her bir tuple'ın ilk elemanı mesafeyi, ikinci eleman da bu mesafe uzaklıktaki noktanın ait olduğu class'ı gösteriyor.
+        # Each tuple contains a distance and a class metric in it. Count the number of each class.
         counter = Counter(b for _, b in k_dists)
 
-        # En çok tekrar eden değeri bulalım.
+        # Find the most repeated class.
         most_common_value, _ = counter.most_common(1)[0]
 
         predicts.append(most_common_value)
